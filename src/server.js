a25caa6adcbeaa995a9bfa0560d669516d325d21
Kimdong11 <87960743+Kimdong11@users.noreply.github.com>
2021-09-16 04:10:14 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import morgar from "morgan";
+import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import vidoeRouter from "./routers/videoRouter"
@@ -7,7 +7,7 @@ import vidoeRouter from "./routers/videoRouter"
 const PORT = 4000;
 
 const app = express();
-const logger = morgar("dev");
+const logger = morgan("dev");
 app.use(logger);
 app.use("/", globalRouter);
 app.use("/videos", vidoeRouter);
