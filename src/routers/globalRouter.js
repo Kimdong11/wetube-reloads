c30c5ddb0b7bbea72294cca707ee9562575b3f29
Kimdong11 <87960743+Kimdong11@users.noreply.github.com>
2021-09-18 07:22:00 +0900

    router and url

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
-
-
-
+import {trending, search} from "../controllers/videoController";
+import {join, login} from "../controllers/userConteroller";
+ 
 const globalRouter = express.Router();
-
-const handleHome = (req, res) => res.send("Home");
-
-globalRouter.get("/", handleHome);
+ 
+globalRouter.get("/", trending);
+globalRouter.get("/join", join);
+globalRouter.get("/login", login);
+globalRouter.get("/search", search);
 
 export default globalRouter;
--- src/server.js
+++ src/server.js
@@ -2,15 +2,15 @@ import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
-import vidoeRouter from "./routers/videoRouter"
+import videoRouter from "./routers/videoRouter"
 
-const PORT = 4000;
+const PORT = 5000;
 
 const app = express();
 const logger = morgan("dev");
 app.use(logger);
 app.use("/", globalRouter);
-app.use("/videos", vidoeRouter);
+app.use("/videos", videoRouter);
 app.use("/users", userRouter)
 
 const handleListening = () => 
